f77ad4d480001fe73179f5503443051afc023f4c
Sundaram-Katare <sundaramkatare53@gmail.com>
2025-01-17 17:54:42 +0530

    Frontend Updated

--- index.js
+++ index.js
@@ -8,12 +8,27 @@ const wind = document.getElementById("wind");
 const humidity = document.getElementById("humidity");
 const pressure = document.getElementById("pressure");
 const clouds = document.getElementById("clouds");
-const sunrise = document.getElementById("sunrise");
-const sunset = document.getElementById("sunset");
+const description = document.getElementById("description");
+const feelsLike = document.getElementById("feelsLike");
 const bg = document.getElementsByTagName("body");
-
 const statsItems = document.querySelectorAll(".item");
 
+
+var i = 0;
+var txt = "Weather Snap";
+var speed = 95;
+
+
+function typeWriter(){
+    if(i < txt.length)
+    {
+        document.getElementById("appName").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter,speed);
+    }
+}
+
+typeWriter();
 
 weatherButton.addEventListener("click", async function (event) {
     event.preventDefault();
@@ -48,62 +63,68 @@ weatherButton.addEventListener("click", async function (event) {
 
         nameOfCity.innerHTML = weatherData.name;
         temp.innerHTML = `${weatherData.temp}`;
-        wind.innerHTML = `Wind : ${weatherData.wind} <i class="fa-solid fa-wind" style="color: #B197FC; height:20px; background-color: black; padding : 10px; border-radius : 5px; "></i> `;
+        wind.innerHTML = `Wind : ${weatherData.wind} <i class="fa-solid fa-wind" style="color: #B197FC; height:20px; background-color: black; padding : 10px; border-radius : 5px; "></i>`;
         humidity.innerHTML = `Humidity : ${weatherData.humidity} <i class="fa-solid fa-droplet" style="color: #74C0FC; height:20px; background-color: black; padding : 10px; border-radius : 5px; "></i>`;
         clouds.innerHTML = `Clouds : ${weatherData.clouds}% <i class="fa-solid fa-cloud" style="color: #ffffff; height:20px; background-color: black; padding : 10px; border-radius : 5px; "></i>`;
-        sunrise.innerHTML = `Sunrise : ${weatherData.sunrise} <i class="fa-regular fa-sun" style="color: #FFD43B; height:20px; background-color: black; padding : 10px; border-radius : 5px; "></i>`
-        sunset.innerHTML = `Sunset : ${weatherData.sunset} <i class="fa-solid fa-sun" style="color: #ff7300; height:20px; background-color: black; padding : 10px; border-radius : 5px; "></i>`
-      
+        pressure.innerHTML = `Pressure : ${weatherData.pressure} <i class="fa-solid fa-temperature-empty" style="color: #63E6BE; height:20px; background-color: black; padding : 10px; border-radius : 5px; "></i>`;
+        feelsLike.innerHTML = `Feels Like : ${weatherData.feelsLike} <i class="fa-solid fa-temperature-full" style="color: #FFD43B; height:20px; background-color: black; padding : 10px; border-radius : 5px; "></i>`
+
         if (temperature < 0) {
-            // Very Cold (Snowy Background)
-            document.body.style.backgroundImage = "url('./images/w4.jpg')";
+            document.body.style.backgroundImage = "url('https://png.pngtree.com/background/20220730/original/pngtree-morning-in-winter-mountains-flat-color-vector-illustration-picture-image_1881242.jpg')";
             document.body.style.backgroundSize = "cover";
             document.body.style.backgroundRepeat = "no-repeat";
-            document.body.style.color = "white";
+            document.body.style.color = "black";
+            temp.style.color = "black";
+            nameOfCity.style.color = "black";
+            appName.style.color = "black";
+
            
             statsItems.forEach((individualItem) => {
-                individualItem.style.color = "white";
-            })
+                individualItem.style.color = "black";
+            });
 
         } else if (temperature < 14) {
-            // Cold (Rainy Background)
             document.body.style.backgroundImage = "url('https://usagif.com/wp-content/uploads/gifs/starfall-gif-46.gif')";
             document.body.style.backgroundSize = "cover";
             document.body.style.backgroundRepeat = "no-repeat";
             document.body.style.color = "white";
+            temp.style.color = "white";
 
             statsItems.forEach((individualItem) => {
                 individualItem.style.color = "white";
             })
 
         } else if (temperature < 25) {
-            // Moderate (Spring Background)
             document.body.style.backgroundImage = "url('https://www.joshmorony.com/media/2016/05/phaser-fog.gif')";
             document.body.style.backgroundSize = "cover";
             document.body.style.backgroundRepeat = "no-repeat";
             document.body.style.color = "black";
+            temp.style.color = "white";
 
             statsItems.forEach((individualItem) => {
                 individualItem.style.color = "white";
             })
         } else if (temperature < 35) {
-            // Warm (Sunny Beach Background)
             document.body.style.backgroundImage = "url('https://cdn.dribbble.com/users/215810/screenshots/2747402/media/6781b8939832022565f0c127dd192c7f.gif')";
             document.body.style.backgroundSize = "cover";
             document.body.style.backgroundRepeat = "no-repeat";
             document.body.style.color = "black";
+
+            temp.style.color = "black";
+            nameOfCity.style.color = "black";
+            appName.style.color = "black";
+
+           
+            statsItems.forEach((individualItem) => {
+                individualItem.style.color = "black";
+            })
         } else {
-            // Very Hot (Desert Background)
             document.body.style.backgroundImage = "url('https://i.gifer.com/embedded/download/YUwc.gif')";
             document.body.style.backgroundSize = "cover";
             document.body.style.backgroundRepeat = "no-repeat";
             document.body.style.color = "black";
         }
-        
-        
-        
 
-        // weatherResult.textContent = "";
     } catch (error) {
         weatherResult.textContent = "Error fetching weather data.";
         console.error(error);
